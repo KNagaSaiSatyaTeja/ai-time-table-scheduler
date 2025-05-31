145dd168366c102bb12ba5215c4daf106ef662d4
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-05-31 12:07:55 +0530

    connection to the mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+try {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB", process.env.MONGODB_URI);
+  });
+} catch (e) {
+  console.log("errror while connecting", e.message);
+}
 
 const app = express();
 const port = 3000;
