0aef4fedcab28388a7db94cfdbd92b472499c0a1
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-05-31 12:05:04 +0530

    backed server js setup created

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
