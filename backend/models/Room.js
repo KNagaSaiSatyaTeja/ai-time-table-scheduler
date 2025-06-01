28ec7e0d33604dc4b33925294868b4ec4401d75d
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-06-01 15:43:32 +0530

    room model added

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -13,6 +13,11 @@ const roomSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    department: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     capacity: {
       type: Number,
       required: true,
@@ -29,10 +34,28 @@ const roomSchema = new mongoose.Schema(
         trim: true,
       },
     ],
+    subjects: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subject",
+        required: true,
+      },
+    ],
     isAvailable: {
       type: Boolean,
       default: true,
     },
+    equipments: [
+      {
+        name: String,
+        quantity: Number,
+        condition: {
+          type: String,
+          enum: ["good", "fair", "maintenance-required"],
+          default: "good",
+        },
+      },
+    ],
   },
   {
     timestamps: true,
